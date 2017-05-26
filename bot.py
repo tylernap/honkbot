fd048c77667bf5790343c9174bc16c5d83d24320
napiet1 <napiet1@nationwide.com>
2017-05-26 15:48:03 -0400

    Added most of the record lookup code. filled in gaps with passes

--- bot.py
+++ bot.py
@@ -6,8 +6,9 @@ from discord.ext import commands
 import random
 import requests
 
+speedrun_api='token'
 google_api='token'
-command_list=["!test","!sleep","!image","!jason","!help"]
+command_list=["!test","!record","!image","!jason","!help"]
 
 client = discord.Client()
 
@@ -23,9 +24,6 @@ def on_message(message):
         chosen = random.choice(list(client.get_all_members()))
         yield from client.send_message(message.channel, "Here is a random member to harass:")
         yield from client.send_message(message.channel, " - ".join([chosen.name,chosen.status.name]))
-    elif message.content.startswith('!sleep'):
-        yield from asyncio.sleep(5)
-        yield from client.send_message(message.channel, "Sleeping. Zzzzz...")
     elif message.content.startswith('!help'):
         yield from client.send_message(message.channel, "Fuck off")
         yield from asyncio.sleep(5)
@@ -52,6 +50,68 @@ def on_message(message):
         r = requests.get("http://quandyfactory.com/insult/json")
         insult = r.json()["insult"]
         yield from client.send_message(message.channel, insult.replace("Thou art","Jason is"))
+    elif message.content.startswith('!record'):
+        search = message.content.lower().split(" ")
+        del search[0]
+        if search:
+            auth = {"Authorization":"Token {}".format(speedrun_api)}
+            query = " ".join(search)
+            results = []
+            if len(search) < 100:
+                baseUrl = "http://www.speedrun.com/api/v1/"
+                apiNext = "".join([baseUrl, "games"])
+                while apiNext:
+                    r = requests.get(apiNext,headers=auth)
+                    for game in r.json()["data"]:
+                        if query in game['names']['international'].lower():
+                            results.append(game)
+                    nextPage = ""
+                    for page in r.json()["pagination"]["links"]:
+                        if "next" in page['rel']:
+                            nextPage = page['uri']
+                    apiNext = nextPage
+                if results:
+                    if len(results) = 1:
+                        gameId = results[0]["id"]
+                        r = requests.get("".join([baseUrl,"games/",gameId]),headers=auth)
+                        gameName = r.json()['data']['names']['international']
+                        r = requests.get(
+                            "".join([baseUrl,"games/",gameId,"/categories"]),headers=auth)
+                        gameCategory = ""
+                        for category in r.json()['data']:
+                            if category['name'].startswith('Any%'):
+                                gameCategory = category['category']
+                                break
+                        if gameCategory:
+                            for link in gameCategory['links']:
+                                if "records" in link['rel']:
+                                    gameRecords = link['uri']
+                            r = requests.get(gameRecords, headers=auth)
+                            run = r.json()['data'][0]['runs'][0]['run']
+                            record = run['times']['realtime'][2:]
+                            userId = run['players'][0]['id']
+                            r = requests.get("".join([baseUrl,"/users/",userId]),headers=auth)
+                            userName = r.json()['data']['names']['international']
+
+                            yield from client.send_message(
+                                message.channel,
+                                "The record for {0} is {1} by {2}".format(gameName,record,userName))
+                
+                        else:
+                            yield from client.send_message(
+                                message.channel,
+                                "There are no Any% records for {}".format(gameName))
+                    elif len(results) < 5:
+                        pass
+                    else:
+                        pass
+                else:
+                    yield from client.send_message(
+                        message.channel,
+                        "No games with that name found!")
+            yield from client.send_message(
+                message.channel,
+                "You gotta give me something to look for...")
     elif message.content.startswith('!'):
         commands = "".join(["Commands are: ",", ".join(command_list)])
         yield from client.send_message(message.channel, commands)
