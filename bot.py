bc87955a747cb00ae419c6a98933d65a4a1f6036
Tyler Napierkowski <tylernap@gmail.com>
2018-04-04 19:54:38 -0400

    Refactored some code. Made it restart on exception

--- bot.py
+++ bot.py
@@ -3,15 +3,20 @@
 import asyncio
 import discord
 from discord.ext import commands
+from discord.errors import Forbidden
 import random
 import requests
 
-speedrun_api='token'
-google_api='token'
-command_list=["!test","!record","!image","!jason","!help"]
+# FIXME: Fill these out with your API tokens
+discord_api=""
+speedrun_api=""
+google_api=""
+
+command_list=["!join","!image","!youtube","!jason","!help"]
+
+lastRecordSearch = ""
 
 client = discord.Client()
-lastRecordSearch = ""
 
 @client.event
 @asyncio.coroutine
@@ -20,37 +25,100 @@ def on_ready():
 
 @client.event
 @asyncio.coroutine
+# TODO: This doesnt work at all lmao.
+# This should send to the individual 
+def on_member_join(member):
+    channel = member.server.default_channel()
+    message = "Welcome {0} to the discord channel!!!"
+    yield from client.send_message(channel, message)
+
+@client.event
+@asyncio.coroutine
 def on_message(message):
     if message.content.startswith('!test'):
         chosen = random.choice(list(client.get_all_members()))
         yield from client.send_message(message.channel, "Here is a random member to harass:")
         yield from client.send_message(message.channel, " - ".join([chosen.name,chosen.status.name]))
+
+    elif message.content.startswith('!join'):
+        allowed_roles = ['OH','MI','KY','PA']
+        if len(message.content.split(" ")) != 2:
+            yield from client.send_message(message.channel, "".join(["Usage: !join [", ", ".join(allowed_roles),"]"]))
+            return
+        role = message.content.split(" ")[1]
+        if role not in allowed_roles:
+            yield from client.send_message(message.channel, "".join(["Allowed roles are: ",", ".join(allowed_roles)]))
+        else:
+            server_roles = message.server.roles
+            for server_role in server_roles:
+                if server_role.name == role:
+                    role_object = server_role
+            try:
+                if message.author.roles:
+                    yield from client.replace_roles(message.author, role_object)
+                else:
+                    yield from client.add_roles(message.author, role_object) 
+                yield from client.send_message(
+                    message.channel, "Adding {0} to {1}".format(message.author.name, role))
+            except Forbidden:
+                yield from client.send_message(
+                    message.channel, "I do not have permissions to assign roles right now. Sorry!")
+
     elif message.content.startswith('!help'):
-        yield from client.send_message(message.channel, "Fuck off")
-        yield from asyncio.sleep(5)
         commands = "".join(["Commands are: ",", ".join(command_list)])
         yield from client.send_message(message.channel, commands)
+    elif message.content.startswith('!youtube'):
+        search = message.content.split(" ")
+        del search[0]
+        if search:
+            query = " ".join(search)
+            if len(query) < 250:
+                url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={0}&key={1}".format(query, google_api)
+                r = requests.get(url)
+                try:
+                    response = r.json()['items'][0]["id"]["videoId"]
+                except IndexError:
+                    yield from client.send_message(message.channel, "Could not find any videos with search {0}".format(query))
+                    return
+
+                if response:
+                    yield from client.send_message(message.channel, "https://youtu.be/{0}".format(response))
+                else:
+                    yield from client.send_message(message.channel, "Could not find any videos with search {0}".format(query))
+                    return
+            else:
+                yield from client.send_message(message.channel, "Query too long!")
+                return
+        else:
+            yield from client.send_message(message.channel, "Usage: !youtube <search terms>")
     elif message.content.startswith('!image'):
         search = message.content.split(" ")
         del search[0]
         if search:
             query = " ".join(search)
-            if len(search) < 100:
+            if len(query) < 150:
                 url = (
                     "https://www.googleapis.com/customsearch/v1?q={}".format(search) +
-                    "&cx=CUSTOMSEARCHCX&searchType=image" +
+                    "&cx=009855409252983983547:3xrcodch8sc&searchType=image" +
                     "&key={}".format(google_api))
                 r = requests.get(url)
-                response = r.json()["items"][0]["link"]
-                yield from client.send_message(message.channel, response)
+                try:
+                    response = r.json()["items"][0]["link"]
+                    yield from client.send_message(message.channel, response)
+                except KeyError:
+                    yield from client.send_message(message.channel, "No results found for {0} :(".format(query))
             else:
                 yield from client.send_message(message.channel, "Query too big!")
         else:
             yield from client.send_message(message.channel, "Usage: !image <search term>")
+
+
     elif message.content.startswith('!jason'):
         r = requests.get("http://quandyfactory.com/insult/json")
         insult = r.json()["insult"]
         yield from client.send_message(message.channel, insult.replace("Thou art","Jason is"))
+
+
     elif message.content.startswith('!record'):
         global lastRecordSearch
         search = message.content.lower().split(" ")
@@ -63,7 +131,7 @@ def on_message(message):
                 baseUrl = "http://www.speedrun.com/api/v1/"
                 apiNext = "".join([baseUrl, "games?name={}".format(query)])
                 while apiNext:
-                    r = requests.get("".join([apiNext,headers=auth)
+                    r = requests.get(apiNext,headers=auth)
                     for game in r.json()["data"]:
                         results.append(game)
                     nextPage = ""
@@ -127,9 +195,21 @@ def on_message(message):
         else:
             yield from client.send_message(
                 message.channel,
-                "You gotta give me something to look for...")
+                "You gotta give me a game to look for...")
+
+    elif "honk" in message.content.lower() and "bot4u" not in message.author.name:
+        yield from client.send_message(message.channel, "HONK!")
+
     elif message.content.startswith('!'):
         commands = "".join(["Commands are: ",", ".join(command_list)])
         yield from client.send_message(message.channel, commands)
 
-client.run('token')
+def main():
+    while True:
+        try:
+            client.run(discord_api)
+        except Exception as e:
+            print("!!! Caught exception: {0}".format(e)
+
+if "__main__" in __name__:
+    main()
