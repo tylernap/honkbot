09fb22b4d70497b337e5d3051e84d3aff2d76820
Michael van Baalen <mystic83@gmail.com>
2018-11-01 17:27:04 -0400

    add in jacket/banner search

--- honkbot.py
+++ honkbot.py
@@ -3,8 +3,10 @@ import discord
 import logging
 import os
 import pytz
-import requests
 import sys
+from bs4 import BeautifulSoup
+import requests
+from typing import Optional
 
 from discord.errors import Forbidden
 import dotenv
@@ -42,7 +44,9 @@ class Honkbot:
             "!ranatalus",
             "!eamuse",
             "!help",
-            "!insult"
+            "!insult",
+            "!jacket",
+            "!banner"
         ]
 
         self.eamuse_maintenance = {
@@ -109,6 +113,10 @@ class Honkbot:
             await self.get_record(message)
         elif message.content.startswith('!eamuse'):
             await self.get_eamuse_maintenance(message)
+        elif message.content.startswith('!jacket'):
+            await self.get_jacket(message)
+        elif message.content.startswith('!banner'):
+            await self.get_banner(message)
 
         elif "honk" in message.content.lower() and message.author != self.client.user:
             # HONK WINS AGAIN
@@ -399,6 +407,72 @@ class Honkbot:
             except Forbidden:
                 await self.client.send_message(
                     message.channel, "I do not have permissions to assign roles right now. Sorry!")
+
+    def search_remy_song(self, query: str) -> Optional[BeautifulSoup]:
+        """
+        Tries to find a certain song on RemyWiki
+
+        MediaWiki tries to match an exact article title. If it doesn't find the
+        exact title, it gives you a page with a list of suggestions. This
+        leverages that by either going to the direct page, or going to the first
+        page in the list of results.
+
+        :param query: a string representing something that's supposed to be a
+            song name to find
+        :return: a BeautifulSoup object representing a RemyWiki page for a song,
+            or None, representing a lack of results
+        """
+        search_data = {'search': f"{query} incategory:\"Songs\""}
+        remy_search = requests.post('http://remywiki.com/index.php', data=search_data)
+        remy_data = BeautifulSoup(remy_search.text, 'html.parser')
+        if remy_data.title.string.lower().startswith(query):
+            return remy_data
+        else:
+            first_result = remy_data.find("ul", {"class": "mw-search-results"})
+            if first_result:
+                song_result = requests.post(f"http://remywiki.com{first_result.li.div.a['href']}")
+                return BeautifulSoup(song_result.text, 'html.parser')
+            else:
+                return None
+
+    def get_remy_image(self, query: str, image_type: str = 'jacket') -> str:
+        """
+        Gets an image (or a message about no image) from a RemyWiki song page.
+
+        This is intended to be used for jackets or banners. This searches the
+        song page for "song name's jacket" or "song name's banner" and returns
+        the image connected to that.
+
+        :param query: a string representing something that's supposed to be a
+            song name to find
+        :param image_type: Either "jacket" or "banner", default "jacket"
+        :return: a response fitting for the bot to return, either the requested
+            image or a message describing what it found instead
+        """
+        remy_data = self.search_remy_song(query)
+        if remy_data is not None:
+            images = remy_data.find_all("div", {"class": "thumbinner"})
+            for image in images:
+                if image_type in image.find("div", {"class": "thumbcaption"}).text:
+                    return f"http://remywiki.com{image.find('img')['src']}"
+
+            song_title = remy_data.find("h1", {"id": "firstHeading"}).text
+            if song_title.lower() == query.lower():
+                return f"{song_title} does not have a {image_type}"
+            else:
+                return f"{query} seems to be the song {song_title} but it does not have a {image_type}"
+        else:
+            return f"Could not find a song that looks like: {query}"
+
+    async def get_jacket(self, message):
+        _, _, query = message.content.partition(" ")
+        response = self.get_remy_image(query, 'jacket')
+        await self.client.send_message(message.channel, response)
+
+    async def get_banner(self, message):
+        _, _, query = message.content.partition(" ")
+        response = self.get_remy_image(query, 'banner')
+        await self.client.send_message(message.channel, response)
 
 
 if "__main__" in __name__:
