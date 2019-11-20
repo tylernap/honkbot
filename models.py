7957d4d64c2e30251d8c27779253f6d18c30477f
unknown <tylernap@gmail.com>
2019-11-19 23:49:30 -0500

    Started models structure. Added psycopg

---
+++ models.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+class DDRCode:
+    def __init__(self, user_id=None):
+        pass
+    def create(self, name=None, ddr_code=None):
+        pass
+    def update(self, **kwargs):
+        pass
+    def delete(self):
+        pass
+
+class IIDXCode:
+    def __init__(self, user_id=None):
+        pass
+    def create(self, name=None, iidx_id=None):
+        pass
+    def update(self, **kwargs):
+        pass
+    def delete(self):
+        pass
