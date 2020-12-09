fff3bf20f39731f1aaa24a46f5fe15e15d4b4bde
Michael van Baalen <mystic83@gmail.com>
2020-12-09 18:49:40 -0500

    allow remywiki to return gallery images

--- bots/remy.py
+++ bots/remy.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 from typing import Optional
 import requests
+import re
 from discord.ext import commands
+
+REMY_URL = "http://remywiki.com"
 
 
 def search_song(query: str) -> Optional[BeautifulSoup]:
@@ -19,17 +22,38 @@ def search_song(query: str) -> Optional[BeautifulSoup]:
         or None, representing a lack of results
     """
     search_data = {'search': f"{query} incategory:\"Songs\""}
-    remy_search = requests.post('http://remywiki.com/index.php', data=search_data)
+    remy_search = requests.get(f"{REMY_URL}/index.php", params=search_data)
     remy_data = BeautifulSoup(remy_search.text, 'html.parser')
-    if remy_data.title.string.lower().startswith(query):
+    if remy_data.title.string.lower().startswith(query.lower()):
         return remy_data
     else:
         first_result = remy_data.find("ul", {"class": "mw-search-results"})
         if first_result:
-            song_result = requests.post(f"http://remywiki.com{first_result.li.div.a['href']}")
+            song_result = requests.post(f"{REMY_URL}{first_result.li.div.a['href']}")
             return BeautifulSoup(song_result.text, 'html.parser')
         else:
             return None
+
+
+def get_image_from_gallery(href: str, image_type: str) -> Optional[str]:
+    """
+    Gets an image from the Gallery page template on RemyWiki
+
+    :param href: The relative url (including the slash) of a gallery page
+    :param image_type: Either "banner" or "jacket"
+
+    :return: A relative URL pointing to an image OR None
+    """
+    gallery_page = requests.get(f"{REMY_URL}{href}")
+    gallery_data = BeautifulSoup(gallery_page.text, 'html.parser')
+    image_sections = gallery_data.find_all("li", {"class": "gallerybox"})
+    for section in image_sections:
+        description = section.find("p").text
+        if image_type in description:
+            img_urls = section.find("img")['srcset'].split(",")
+            largest_image = img_urls[-1]
+            return largest_image.split(" ")[1]  # space,url,size
+    return None
 
 
 def get_image(query: str, image_type: str = 'jacket') -> str:
@@ -46,19 +70,26 @@ def get_image(query: str, image_type: str = 'jacket') -> str:
     :return: a response fitting for the bot to return, either the requested
         image or a message describing what it found instead
     """
-    remy_data = search_song(query)
-    if remy_data is not None:
-        images = remy_data.find_all("div", {"class": "thumbinner"})
+    song_page = search_song(query)
+    if song_page:
+        # First try to get the image from the Gallery
+        gallery = song_page.find("a", href=re.compile(r"Gallery"))
+        if gallery:
+            image_url = get_image_from_gallery(gallery["href"], image_type)
+            if image_url:
+                return f"{REMY_URL}{image_url}"
+        # If there is no Gallery, try to find it on the page
+        images = song_page.find_all("div", {"class": "thumbinner"})
         for image in images:
             if image_type in image.find("div", {"class": "thumbcaption"}).text:
-                return f"http://remywiki.com{image.find('img')['src']}"
-
-        song_title = remy_data.find("h1", {"id": "firstHeading"}).text
+                return f"{REMY_URL}{image.find('img')['src']}"
+        # If we can't find it on either, give out a message
+        song_title = song_page.find("h1", {"id": "firstHeading"}).text
         if song_title.lower() == query.lower():
             return f"{song_title} does not have a {image_type}"
         else:
             return f"{query} seems to be the song {song_title} but it does not have a {image_type}"
-    else:
+    else:  # No song page
         return f"Could not find a song that looks like: {query}"
 
 
